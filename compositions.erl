18d59cd55890c131199db73fbb0b728c80eee83e
quantumpotato <quantumpotato@gmail.com>
2017-02-04 09:54:29 -0500

    Note

--- compositions.erl
+++ compositions.erl
@@ -2,6 +2,8 @@
 -export([compositions/1]).
 
 -define(COMPOSITIONS, #{"Ship" => [position]}).
+% -define(COMPOSITIONS, #{"Ship" => [matter]}).
+where matter has position, velocity
 
 compositions(Name) ->
   maps:get(Name, ?COMPOSITIONS).
